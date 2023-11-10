4f2efccfba5a8533b66069dd08c07f960f812c67
Aleix-White1 <aleixblanco10@gmail.com>
2023-11-10 10:47:15 +0100

    Create new Exercise

---
+++ src/Ejercicio3/NombresFamiliares.java
@@ -0,0 +1,23 @@
+package Ejercicio3;
+
+import java.util.Scanner;
+
+public class NombresFamiliares {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce primer nombre de un familiar/amigo: ");
+        String primerNombre = scanner.nextLine();
+
+        System.out.println("Introduce segundo nombre de un familiar/amigo: ");
+        String segundoNombre = scanner.nextLine();
+
+        System.out.println("Introduce tercer nombre de un familiar/amigo: ");
+        String tercerNombre = scanner.nextLine();
+
+        String formatPrimerNombre;
+
+
+
+    }
+}
